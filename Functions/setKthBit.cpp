e0db0be51decd8c016e4bee33132a55e2226d496
Md Aman <aman.nit.cse@gmail.com>
2024-09-26 17:30:01 +0530

    done

--- Functions/setKthBit.cpp
+++ Functions/setKthBit.cpp
@@ -4,67 +4,23 @@
 
 using namespace std;
 
-void decimalToBinary(int n, vector<int> &ans)
+int setKthBit(int &n, int k)
 {
-
-    while (n > 0)
-    {
-        int digit = n % 2;
-        ans.push_back(digit);
-        n /= 2;
-    }
-}
-
-void setKthBit(vector<int> &ans, int k)
-{
-
-    if (ans[k + 1] == 0)
-    {
-        ans[k + 1] = 1;
-    }
-    else
-    {
-        ans[k + 1] = 1;
-    }
-}
-
-int binaryToDecimal(vector<int> &ans)
-{
-    int decimalValue = 0;
-    int length = ans.size();
-
-    for (int i = 0; i < length; i++)
-    {
-        // Convert character '0' or '1' to int and multiply by the corresponding power of 2
-        if (ans[length - 1 - i] == '1')
-        {
-            decimalValue += pow(2, i);
-        }
-    }
-
-    return decimalValue;
+    int mask = 1 << k;
+    int ans = n | mask;
+    return ans;
 }
 
 int main()
 {
     int n, k;
-    vector<int> ans;
+
     cout << "Enter an integer:-";
     cin >> n;
     cout << "Enter the value of k:-";
     cin >> k;
-    decimalToBinary(n, ans);
-    for (int i = ans.size() - 1; i >= 0; i--)
-    {
-        cout << ans[i] << " ";
-    }
-    cout << endl;
-    setKthBit(ans, k);
-    for (int i = ans.size() - 1; i >= 0; i--)
-    {
-        cout << ans[i] << " ";
-    }
-    cout << endl;
-    cout << binaryToDecimal(ans);
+
+    int finalAns = setKthBit(n, k);
+    cout << "Answer is " << finalAns;
     return 0;
 }

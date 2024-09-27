3db3189441bfe85316894282f848c3ceaee46f78
Md Aman <aman.nit.cse@gmail.com>
2024-09-27 08:34:15 +0530

    update

--- Patterns/halfPyramid.cpp
+++ Patterns/halfPyramid.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 using namespace std;
 
-//  *  *  *  *  *
-//  *  *  *  *  *
-//  *  *  *  *  *
-//  *  *  *  *  *
-//  *  *  *  *  *
-
 int main()
 {
     int n;
@@ -14,7 +8,7 @@ int main()
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < i + 1; j++)
         {
             cout << " * ";
         }

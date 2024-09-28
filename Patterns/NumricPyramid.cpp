4f5439dc285e1e0b7aa071e83979db04bfab884b
Md Aman <aman.nit.cse@gmail.com>
2024-09-28 19:20:48 +0530

    underProcess

---
+++ Patterns/NumricPyramid.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a number:- ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            /* code */
+        }
+        for (int k = 0; k < n; k++)
+        {
+            /* code */
+        }
+    }
+
+    return 0;
+}

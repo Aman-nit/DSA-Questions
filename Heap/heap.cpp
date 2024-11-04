962acdf50f607a48ea525874b9d07e7430b92da0
Md Aman <aman.nit.cse@gmail.com>
2024-11-04 11:47:31 +0530

    update

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -44,23 +44,33 @@ public:
 int main()
 {
     heap h;
-    h.arr[0] = -1;
-    h.arr[1] = 100;
-    h.arr[2] = 50;
-    h.arr[3] = 60;
-    h.arr[4] = 40;
-    h.arr[5] = 45;
-    h.size = 5;
+    // INSERING IN A HEAP
+    //  h.arr[0] = -1;
+    //  h.arr[1] = 100;
+    //  h.arr[2] = 50;
+    //  h.arr[3] = 60;
+    //  h.arr[4] = 40;
+    //  h.arr[5] = 45;
+    //  h.size = 5;
+
+    // CREATION OF HEAP
+
+    h.insert(50);
+    h.insert(30);
+    h.insert(70);
+    h.insert(40);
+    h.insert(80);
+    h.insert(100);
+
+    // cout << "Printing the heap " << endl;
+    // for (int i = 0; i <= h.size; i++)
+    // {
+    //     cout << h.arr[i] << " ";
+    // }
+    // cout << endl;
 
     cout << "Printing the heap " << endl;
-    for (int i = 0; i <= h.size; i++)
-    {
-        cout << h.arr[i] << " ";
-    }
-    cout << endl;
-    h.insert(110);
-    cout << "Printing the heap " << endl;
-    for (int i = 0; i <= h.size; i++)
+    for (int i = 1; i <= h.size; i++)
     {
         cout << h.arr[i] << " ";
     }

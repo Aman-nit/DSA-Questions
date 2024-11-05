6052baa7e5a1ca2659f632ec9011fce88f07434d
Md Aman <aman.nit.cse@gmail.com>
2024-11-05 14:55:24 +0530

    added

---
+++ Heap/isMaxHeap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+// pair<bool, int> solve(node *root)
+// {
+//     if (root == NULL)
+//     {
+//         pair<bool, int> p = make_pair(true, INT8_MIN);
+//         return p;
+//     }
+//     if (root->left == NULL && root->right == NULL)
+//     {
+//         // leaf node
+//         pair
+//     }
+// }

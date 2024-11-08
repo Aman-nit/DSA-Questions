8255502f124ebc28f2c92bd69a9e30545dfdf473
Md Aman <aman.nit.cse@gmail.com>
2024-11-08 15:53:54 +0530

    added

--- Tries/tries.cpp
+++ Tries/tries.cpp
@@ -1,1 +1,64 @@
+// Tries are tree with multiple child
 
+#include <iostream>
+using namespace std;
+
+class triNode
+{
+public:
+    char data;
+    triNode *children[26];
+    bool isTerminal;
+
+    triNode(char ch)
+    {
+        data = ch;
+        for (int i = 0; i < 26; i++)
+        {
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+void insertWord(triNode *root, string word)
+{
+    cout << "Inserting " << word << endl;
+
+    // Base case
+    if (word.length() == 0)
+    {
+        root->isTerminal = true;
+        return;
+    }
+    // Find index of the first char of the word
+    char ch = word[0];
+    int index = ch - 'A';
+    triNode *child;
+
+    // first char of word  is present in root node
+    if (root->children[index] != NULL)
+    {
+        child = root->children[index];
+    }
+    // first char of word  is not present in root node
+    else
+    {
+        child = new triNode(ch);
+        root->children[index] = child;
+    }
+
+    // first char of the word inserted successfully
+
+    // Inserting remaining char of the word by using recursion
+    insertWord(child, word.substr(1));
+}
+
+int main()
+{
+
+    triNode *root = new triNode('\0');
+
+    insertWord(root, "coding ");
+
+    return 0;
+}

ad84b79ec34cd6f13f065fd76a89d586c69c367c
Md Aman <aman.nit.cse@gmail.com>
2024-11-09 15:37:32 +0530

    added

--- Tries/tries.cpp
+++ Tries/tries.cpp
@@ -20,45 +20,100 @@ public:
         isTerminal = false;
     }
 };
+// void insertWord(triNode *root, string word)
+// {
+//     cout << "Inserting " << word << endl;
+
+//     // Base case
+//     if (word.length() == 0)
+//     {
+//         root->isTerminal = true;
+//         return;
+//     }
+//     // Find index of the first char of the word
+//     char ch = word[0];
+//     int index = ch - 'A';
+//     triNode *child;
+
+//     // first char of word  is present in root node
+//     if (root->children[index] != NULL)
+//     {
+//         child = root->children[index];
+//     }
+//     // first char of word  is not present in root node
+//     else
+//     {
+//         child = new triNode(ch);
+//         root->children[index] = child;
+//     }
+
+//     // first char of the word inserted successfully
+
+//     // Inserting remaining char of the word by using recursion
+//     insertWord(child, word.substr(1));
+// }
+
 void insertWord(triNode *root, string word)
 {
-    cout << "Inserting " << word << endl;
+    triNode *current = root;
+    for (char ch : word)
+    {
+        int index = tolower(ch) - 'a'; // Handle lowercase
 
-    // Base case
+        if (current->children[index] == NULL)
+        {
+            current->children[index] = new triNode(ch);
+        }
+
+        current = current->children[index];
+    }
+    current->isTerminal = true;
+}
+bool searchWord(triNode *root, string word)
+{
+    // base casae
     if (word.length() == 0)
     {
-        root->isTerminal = true;
-        return;
+        return root->isTerminal;
     }
-    // Find index of the first char of the word
     char ch = word[0];
-    int index = ch - 'A';
+    int index = ch - 'a';
     triNode *child;
 
-    // first char of word  is present in root node
+    // present
     if (root->children[index] != NULL)
     {
         child = root->children[index];
     }
-    // first char of word  is not present in root node
     else
     {
-        child = new triNode(ch);
-        root->children[index] = child;
+        return false;
     }
 
-    // first char of the word inserted successfully
-
-    // Inserting remaining char of the word by using recursion
-    insertWord(child, word.substr(1));
+    // recursion will handel other char of the words
+    return searchWord(child, word.substr(1));
 }
-
 int main()
 {
 
     triNode *root = new triNode('\0');
 
-    insertWord(root, "coding ");
+    insertWord(root, "coding");
+    insertWord(root, "code");
+    insertWord(root, "coder");
+    insertWord(root, "codehlp");
+    insertWord(root, "baby");
+    insertWord(root, "jaan");
+    insertWord(root, "babu");
+
+    if (searchWord(root, "coder"))
+    {
+        cout << "Present..." << endl;
+    }
+    else
+    {
+        cout << " NOt Present..." << endl;
+    }
 
     return 0;
 }

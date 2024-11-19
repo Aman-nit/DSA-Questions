f874eeffa2beb055cf025fe82936eade78a562b6
Md Aman <aman.nit.cse@gmail.com>
2024-11-19 22:32:20 +0530

    added

--- DynamicProgramming/dp.cpp
+++ DynamicProgramming/dp.cpp
@@ -1,1 +1,26 @@
+/*
+// DYNAMIC PROGRAMMING
+// is a programming techicke where we lern not to claculate again and again same subproblem
 
+// Approaches
+// 1.Top  Down approacf
+// Recursion +memoisation
+
+// 2.Bottom up
+// Tabulation method
+
+// 3.Space Optimisation
+
+// WHEN TO APPPLY..?
+// 1.Overlapping sub Problem present(same problem ko bar bar solve krna)
+// 2.Optimal SUbstructure(Optimal sol of bigger problem can be calculated using optimal solution of smaller problem )
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello ";
+}

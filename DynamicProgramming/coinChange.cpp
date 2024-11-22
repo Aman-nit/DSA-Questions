c64a99537999865fed1d75963d4e76760e534dff
Md Aman <aman.nit.cse@gmail.com>
2024-11-22 18:22:07 +0530

    update cases

--- DynamicProgramming/coinChange.cpp
+++ DynamicProgramming/coinChange.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 using namespace std;
 
-int coinChange(vector<int> &coins, int amount)
+// Recursion
+int coinChangeRecursion(vector<int> &coins, int amount)
 {
     // Base case
     if (amount == 0)
@@ -17,13 +18,67 @@ int coinChange(vector<int> &coins, int amount)
     int mini = INT16_MAX;
     for (int i = 0; i < coins.size(); i++)
     {
-        int ans = coinChange(coins, amount - coins[i]);
+        int ans = coinChangeRecursion(coins, amount - coins[i]);
         if (ans != INT16_MAX)
         {
             mini = min(mini, 1 + ans);
         }
     }
     return mini;
+}
+
+// Memoisation
+int coinChangeMemo(vector<int> &coins, int amount, vector<int> &dp)
+{
+
+    // Base case
+    if (amount == 0)
+    {
+        return 0;
+    }
+    if (amount < 0)
+    {
+        return INT16_MAX;
+    }
+    // Ans already exists
+    if (dp[amount] != -1)
+    {
+        return dp[amount];
+    }
+
+    int mini = INT16_MAX;
+    for (int i = 0; i < coins.size(); i++)
+    {
+        int ans = coinChangeMemo(coins, amount - coins[i], dp);
+        if (ans != INT16_MAX)
+        {
+            mini = min(mini, 1 + ans);
+        }
+    }
+    dp[amount] = mini;
+    return dp[amount];
+}
+
+// Tabulation method
+int coinChangeTabul(vector<int> &coins, int amount)
+{
+    vector<int> dp(amount + 1, INT16_MAX);
+
+    dp[0] = 0;
+
+    for (int i = 1; i <= amount; i++)
+    {
+        for (int j = 0; j < coins.size(); j++)
+        {
+            int ans = coinChangeMemo(coins, amount - coins[j], dp);
+            if (i - coins[j] >= 0 && dp[i - coins[j]] != INT16_MAX)
+            {
+                int ans = dp[i - coins[j]];
+                dp[i] = min(dp[i], 1 + ans);
+            }
+        }
+    }
+    return dp[amount];
 }
 
 int main()
@@ -39,15 +94,38 @@ int main()
     }
     cout << "Enter target amount:-";
     cin >> amount;
-    int ans = coinChange(coins, amount);
+    vector<int> dp(amount + 1, -1);
+
+    cout << "Answer using Recursion ..." << endl;
+    int ans = coinChangeRecursion(coins, amount);
     if (ans == INT16_MAX)
     {
-        cout << "Sorry there is no possiblity ...";
+        cout << "Sorry there is no possiblity ..." << endl;
     }
     else
     {
-        cout << ans;
+        cout << ans << endl;
+    }
+    cout << "Answer using Recursion + Memoisation ..." << endl;
+    int ans2 = coinChangeMemo(coins, amount, dp);
+    if (ans2 == INT16_MAX)
+    {
+        cout << "Sorry there is no possiblity ..." << endl;
+    }
+    else
+    {
+        cout << ans2 << endl;
     }
 
+    cout << "Answer using Tabulation  ..." << endl;
+    int ans3 = coinChangeTabul(coins, amount);
+    if (ans3 == INT16_MAX)
+    {
+        cout << "Sorry there is no possiblity ..." << endl;
+    }
+    else
+    {
+        cout << ans3 << endl;
+    }
     return 0;
 }
